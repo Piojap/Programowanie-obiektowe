a5ab55395275506327c9b9a6e12c17aa7d56be8e
Piotr Japoł <72450415+Piojap@users.noreply.github.com>
2022-05-15 21:09:24 +0200

    Add files via upload

---
+++ LAB2_PJ/Products.cs
@@ -0,0 +1,62 @@
+﻿namespace LAB2_PJ
+{
+    abstract class Product : IThing
+    {
+        public Product(string name)
+        {
+            this.name = name;
+
+        }
+
+        protected string name;
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public abstract void Print(string prefix);
+    }
+
+    class Fruit : Product
+    {
+        int count;
+
+        public int Count
+        {
+            get {return count;}
+        }
+
+        public Fruit(string name, int count) : base(name)
+        {
+            this.count = count;
+        }
+
+        public override void Print(string prefix)
+        {
+            Console.WriteLine($"{prefix}{this.name} ({this.count} fruits)");
+        }
+    }
+
+    class Meat : Product
+
+    {
+        double weigth;
+
+        public double Weigth
+        {
+            get {return weigth;}
+        }
+
+        public Meat(string name, double weigth) : base(name)
+        {
+            this.weigth = weigth;
+        }
+
+        public override void Print(string prefix)
+        {
+            Console.WriteLine(prefix + this.name + " (" + this.weigth + " kg)");
+            Console.WriteLine($"{prefix}{this.name} ({this.weigth} kg)");
+        }
+    }
+}
---
+++ LAB2_PJ/Shop.cs
@@ -0,0 +1,72 @@
+﻿namespace LAB2_PJ
+{
+//PROGRAM - SHOP VALUES
+    class Program
+    {
+        public static void Main()
+        {
+            Seller treacher = new Seller("Jan Kowalski", 50);
+
+            Buyer buyer1 = new Buyer("Jaś Fasola 1", 25);
+            Buyer buyer2 = new Buyer("Jaś Fasola 2", 21);
+            Buyer buyer3 = new Buyer("Jaś Fasola 3", 23);
+
+            buyer1.AddProduct(new Fruit("Apple", 6));
+            buyer1.AddProduct(new Meat("Fish", 0.5));
+
+            Person[] persons = { treacher, buyer1, buyer2, buyer3 };
+
+            Product[] products = {
+                new Fruit("Apple", 1000),
+                new Fruit("Banana", 700),
+                new Fruit("Orange", 500),
+                new Meat("Fish", 100.0),
+                new Meat("Beef", 75.0)
+            };
+
+            Shop shop = new Shop("Super Market", persons, products);
+
+            shop.Print();
+        }
+    }
+
+    interface IThing
+    {
+        string Name { get; }
+    }
+
+    class Shop
+    {
+        private string name;
+        private Person[] people;
+        private Product[] products;
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public Shop(string name, Person[] people, Product[] products)
+        {
+            this.name = name;
+            this.people = people;
+            this.products = products;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Shop: {this.name}");
+            Console.WriteLine("-- People: --");
+            foreach (var p in people)
+            {
+                p.Print("\t");
+            }
+            Console.WriteLine("-- Products: --");
+            foreach (var p in products)
+            {
+                p.Print("\t");
+            }
+
+        }
+    }
+}
