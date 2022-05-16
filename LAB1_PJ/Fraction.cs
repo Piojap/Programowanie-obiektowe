320eb1a09eee86fa1d7ff105ba3bbf1bdf4effa1
Piotr Japoł <72450415+Piojap@users.noreply.github.com>
2022-05-16 23:02:40 +0200

    Add files via upload

---
+++ LAB1_PJ/Fraction.cs
@@ -0,0 +1,80 @@
+﻿namespace Lab1NS
+{
+    public class Fraction : IComparable<Fraction>, IEquatable<Fraction>
+    {
+        private int numerator { get; }
+        private int denominator { get; }
+
+        public Fraction()
+        {
+            this.numerator = 1;
+            this.denominator = 2;
+        }
+
+        public Fraction(int numerator, int denominator)
+        {
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+
+        public Fraction(Fraction value)
+        {
+            this.numerator = value.numerator;
+            this.denominator = value.denominator;
+        }
+
+        public int CompareTo(Fraction other)
+        {
+            if ((double)this.numerator / (double)this.denominator == (double)other.numerator / (double)other.denominator) return 0;
+            if ((double)this.numerator / (double)this.denominator > (double)other.numerator / (double)other.denominator) return +1;
+            if ((double)this.numerator / (double)this.denominator < (double)other.numerator / (double)other.denominator) return -1;
+
+            return 0;
+        }
+
+        public bool Equals(Fraction other)
+        {
+            if (other != this) return false;
+            if (other == this) return true;
+
+            return Object.Equals(this.numerator, other.numerator) && Object.Equals(this.denominator, other.denominator);
+        }
+
+        public double RoundUp()
+        {
+            double result = (double)this.numerator / (double)this.denominator;
+            double remainder = result % 1;
+            result -= remainder;
+            if (result >= 0) result += 1;
+            else result -= 1;
+            return result;
+        }
+
+        public double RoundDown()
+        {
+            double result = (double)this.numerator / (double)this.denominator;
+            double remainder = result % 1;
+            result -= remainder;
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return $"Numerator equals: {this.numerator} Denominator equals: {this.denominator}";
+        }
+
+        public static Fraction operator +(Fraction a) => a;
+        public static Fraction operator -(Fraction a) => new Fraction(-a.numerator, a.denominator);
+        public static Fraction operator +(Fraction a, Fraction b) => new Fraction(a.numerator * b.denominator + b.numerator * a.denominator, a.denominator * b.denominator);
+        public static Fraction operator -(Fraction a, Fraction b) => a + (-b);
+        public static Fraction operator *(Fraction a, Fraction b) => new Fraction(a.numerator * b.numerator, a.denominator * b.denominator);
+        public static Fraction operator /(Fraction a, Fraction b)
+        {
+            if (b.numerator == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return new Fraction(a.numerator * b.denominator, a.denominator * b.numerator);
+        }
+    }
+}
---
+++ LAB1_PJ/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1NS
+{
+    public class Program
+    {
+
+        public static void Main()
+        {
+
+            Fraction[] fractions = new Fraction[]
+            {
+                new Fraction(50, 21),
+                new Fraction(-50, 8),
+                new Fraction(127, 521),
+            };
+
+            Console.WriteLine(fractions[1].RoundUp());
+
+            Console.WriteLine(fractions[1].RoundDown());
+
+            Console.WriteLine("Nie posortowane: ");
+            for (int i = 0; i < fractions.Length; ++i)
+                Console.WriteLine(fractions[i].ToString());
+
+            Array.Sort(fractions);
+
+            Console.WriteLine("Posortowane od najmiejszego: ");
+            for (int i = 0; i < fractions.Length; ++i)
+                Console.WriteLine(fractions[i].ToString());
+
+            Console.WriteLine(fractions[0].Equals(fractions[0]));
+        }
+    }
+}
